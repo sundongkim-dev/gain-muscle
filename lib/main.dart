3cdceaa19f45e377b9f500caf419e1361176c795
mok03127 <mok03127@naver.com>
2021-12-29 21:47:26 +0900

    update icons

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ Future<bool> fetchData() async {
   bool data = false;
 
   // Change to API call
-  await Future.delayed(Duration(seconds: 3), () {
+  await Future.delayed(Duration(seconds: 1), () {
     data = true;
   });
   return data;
