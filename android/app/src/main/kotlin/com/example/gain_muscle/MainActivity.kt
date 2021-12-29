582d4aa91cdf307e9d809496c7efa4172911fed6
mok03127 <mok03127@naver.com>
2021-12-29 19:35:34 +0900

    commit inital files

---
+++ android/app/src/main/kotlin/com/example/gain_muscle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gain_muscle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:gain_muscle/splashScreen.dart';
+
+Future<void> main() async {
+  bool data = await fetchData();
+  print(data);
+
+  runApp(const MyApp());      // 앱 시작
+}
+
+// 앱 메인페이지
+// 앱 디자인: 위젯 짜깁기
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // home: Image.asset('img/splashImg/splashImg.png')
+      /*home: Center(
+        child: Container(width:50, height:50, color:Colors.blue), // 사이즈 단위는 픽셀이 아닌 LP, 50LP == 1.2cm
+      )*/
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+        bottomNavigationBar: BottomAppBar(child: Text('최형진')),
+      )
+
+    );
+  }
+}
+// 글자: Text
+// 아이콘: Icon(Icons.아이콘이름)
+// 이미지: Image.asset('경로')
+// 네모박스: Container()
+
+Future<bool> fetchData() async {
+  bool data = false;
+
+  // Change to API call
+  await Future.delayed(Duration(seconds: 3), () {
+    data = true;
+  });
+  return data;
+}
