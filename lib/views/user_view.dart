5787231f58f9139afb9d13b010a1a15c6ec0fbcd
mok03127 <mok03127@naver.com>
2022-01-08 16:39:10 +0900

    create logout function and userviewimg

--- lib/views/user_view.dart
+++ lib/views/user_view.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+User? user = FirebaseAuth.instance.currentUser;
 
 class userView extends StatefulWidget {
   const userView({Key? key}) : super(key: key);
@@ -8,10 +11,67 @@ class userView extends StatefulWidget {
 }
 
 class _userViewState extends State<userView> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('사용자 설정 페이지'),
-    );
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "  프로필",
+                      style: TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    Center(
+                      child: Image(
+                        image: AssetImage("assets/Img/userviewImg/strong.png"),
+                      ),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "별명 들어가야 하는 부분",
+                            textAlign: TextAlign.center,
+                          ),
+                        ]
+                    ),
+                    Row(
+                      children: [
+                        Expanded(child: Divider()),
+                        Expanded(child: Divider()),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ElevatedButton.icon(
+                                onPressed: () {
+                                  FirebaseAuth.instance.signOut();
+                                },
+                                label: Text("로그아웃"),
+                                icon: Icon(Icons.logout),
+                            ),
+                          ],
+                        ),
+                    ),
+                  ]
+              ),
+            )
+          ],
+        ));
   }
 }
