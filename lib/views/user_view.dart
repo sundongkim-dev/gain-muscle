790f18e748fca3e94e5f48df19c9bfb3984e3cb6
HyeongJinChoi <gagu1999@kaist.ac.kr>
2022-01-08 21:32:38 +0900

    유저 정보페이지 수정

--- lib/views/user_view.dart
+++ lib/views/user_view.dart
@@ -11,67 +11,62 @@ class userView extends StatefulWidget {
 }
 
 class _userViewState extends State<userView> {
-
+  String name = FirebaseAuth.instance.currentUser!.displayName as String;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const Text(
+          "  프로필",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
-        body: CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "  프로필",
-                      style: TextStyle(
-                          fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    Center(
-                      child: Image(
-                        image: AssetImage("assets/Img/userviewImg/strong.png"),
-                      ),
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "별명 들어가야 하는 부분",
-                            textAlign: TextAlign.center,
-                          ),
-                        ]
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child: Divider()),
-                        Expanded(child: Divider()),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            ElevatedButton.icon(
-                                onPressed: () {
-                                  FirebaseAuth.instance.signOut();
-                                },
-                                label: Text("로그아웃"),
-                                icon: Icon(Icons.logout),
-                            ),
-                          ],
-                        ),
-                    ),
-                  ]
-              ),
-            )
+        Center(
+          child: Image(
+            image: AssetImage("assets/Img/userviewImg/strong.png"),
+            width: 200,
+            height: 200,
+          ),
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            // "별명 들어가야 하는 부분",
+            name,
+            textAlign: TextAlign.center,
+          ),
+        ]),
+        Row(
+          children: [
+            Expanded(child: Divider()),
+            Expanded(child: Divider()),
           ],
-        ));
+        ),
+        // Padding(
+        //   padding: EdgeInsets.all(20),
+        //   child: Row(
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     children: [
+        //       ElevatedButton.icon(
+        //         onPressed: () {
+        //           FirebaseAuth.instance.signOut();
+        //         },
+        //         label: Text("로그아웃"),
+        //         icon: Icon(Icons.logout),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => FirebaseAuth.instance.signOut(),
+        child: Icon(Icons.logout),
+        tooltip: "로그아웃",
+      ),
+    );
   }
 }
