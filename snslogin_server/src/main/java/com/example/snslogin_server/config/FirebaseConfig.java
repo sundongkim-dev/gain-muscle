eeb487cceab0058d4f0d8eef339e3850763a1c29
mok03127 <mok03127@naver.com>
2022-01-11 21:33:08 +0900

    update spring server for kakao login

--- snslogin_server/src/main/java/com/example/snslogin_server/config/FirebaseConfig.java
+++ snslogin_server/src/main/java/com/example/snslogin_server/config/FirebaseConfig.java
@@ -1,14 +1,17 @@
 package com.example.snslogin_server.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
 
 @Configuration
 public class FirebaseConfig {
@@ -16,7 +19,7 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/gainMuscleLoginSecret.json");
+            FileInputStream serviceAccount =  new FileInputStream("src/main/resources/testsnsloginsecret.json");
             FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
             FirebaseApp.initializeApp(options);
         } catch (FileNotFoundException e) {
@@ -28,3 +31,6 @@ public class FirebaseConfig {
     }
 
 }
+
+
+
