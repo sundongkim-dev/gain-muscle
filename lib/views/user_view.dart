6e61373c69f6f51374ac34ae40e256313a91cc39
Paul_Kim <mok03127@naver.com>
2022-01-17 18:19:11 +0900

    update user_view

--- lib/views/user_view.dart
+++ lib/views/user_view.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gain_muscle/src/pages/developerInfo.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 User? user = FirebaseAuth.instance.currentUser;
 
@@ -111,7 +111,60 @@ class _userViewState extends State<userView> {
                 SizedBox(
                   height: 4,
                 ),
-                stuff('회원 탈퇴하기', Icons.palette, developerInfo()),
+                InkWell(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.elderly,
+                          color: Colors.black45,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(child:
+                        Text(
+                          "회원 탈퇴하기", style: TextStyle(fontSize: 18, color: Colors.black45),
+                        )),
+                      ],
+                    ),
+                    onTap: () async {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => deleteUser()));
+                    }
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  indent: 10,
+                  thickness: 1.5,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                InkWell(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.logout,
+                          color: Colors.black45,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(child:
+                        Text(
+                          "로그아웃 하기", style: TextStyle(fontSize: 18, color: Colors.black45),
+                        )),
+                      ],
+                    ),
+                    onTap: () => FirebaseAuth.instance.signOut(),
+                ),
                 SizedBox(
                   height: 20,
                 ),
@@ -119,7 +172,39 @@ class _userViewState extends State<userView> {
                   "\n     쓰담쓰담 한마디\n",
                   style: TextStyle(color: Colors.black45),
                 ),
-                stuff('피드백 보내기', Icons.chat, developerInfo()),
+                InkWell(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.chat,
+                          color: Colors.black45,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(child:
+                        Text(
+                          "피드백 보내기", style: TextStyle(fontSize: 18, color: Colors.black45),
+                        )),
+                      ],
+                    ),
+                    onTap: () async {
+                      final Uri params = Uri(
+                        scheme: 'mailto',
+                        path: 'mok03127@gmail.com',
+                        //query:
+                      );
+                      String url = params.toString();
+                      if(await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        print("Can't launch $url");
+                      }
+                    }
+                ),
                 SizedBox(
                   height: 4,
                 ),
@@ -138,7 +223,40 @@ class _userViewState extends State<userView> {
                   "\n     득근이와 소통하기\n",
                   style: TextStyle(color: Colors.black45),
                 ),
-                stuff('카페 구경가기', Icons.local_cafe_rounded, developerInfo()),
+                InkWell(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.local_cafe_rounded,
+                          color: Colors.black45,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(child:
+                        Text(
+                          "카페 구경가기", style: TextStyle(fontSize: 18, color: Colors.black45),
+                        )),
+                      ],
+                    ),
+                    onTap: () async {
+                      const url = 'https://cafe.naver.com/gainmuscle';
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                          forceSafariVC: false,
+                          forceWebView: false,
+                          enableJavaScript: true,
+                        );
+                      }
+                      else {
+                        print("!!!");
+                      }
+                    }
+                ),
                 SizedBox(
                   height: 4,
                 ),
@@ -149,7 +267,40 @@ class _userViewState extends State<userView> {
                 SizedBox(
                   height: 4,
                 ),
-                stuff('득근이 인스타그램', Icons.camera_alt_outlined, developerInfo()),
+                InkWell(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.camera_alt_outlined,
+                          color: Colors.black45,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(child:
+                        Text(
+                          "득근이 인스타그램", style: TextStyle(fontSize: 18, color: Colors.black45),
+                        )),
+                      ],
+                    ),
+                    onTap: () async {
+                      const url = 'https://www.instagram.com/gain._.muscle';
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                          forceSafariVC: false,
+                          forceWebView: false,
+                          enableJavaScript: true,
+                        );
+                      }
+                      else {
+                        print("!!!");
+                      }
+                    }
+                ),
                 SizedBox(
                   height: 4,
                 ),
@@ -166,11 +317,43 @@ class _userViewState extends State<userView> {
           ),
         ),
       ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => FirebaseAuth.instance.signOut(),
-        child: Icon(Icons.logout),
-        tooltip: "로그아웃",
+    );
+  }
+}
+
+class deleteUser extends StatelessWidget {
+  const deleteUser({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("득근득근 회원 탈퇴"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("정말 탈퇴하시겠어요?\n탈퇴 과정엔 계정에 따라 재인증이 필요하며 탈퇴 후엔 그동안의 기록들을 절대로 복구할 수 없습니다!"),
+        ],
       ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () async {
+            Navigator.pop(context);
+          },
+          child: Text("취소"),
+        ),
+        TextButton(
+          onPressed: () async {
+            try{
+              User user = await FirebaseAuth.instance.currentUser!;
+              user.delete();
+            } on FirebaseAuthException catch (e){
+              print(e);
+            }
+            Navigator.pop(context);
+          },
+          child: Text("확인"),),
+      ],
     );
   }
 }
